38d5f3fb69d596af9188c5dfe9f74ad094dfb669
dominik pytlewski <dominik@pytlewski.eu>
2020-03-25 08:26:40 +0100

    lazy popup fragment

--- AndroidApp/app/src/main/java/pl/todoit/IndustrialWebViewWithQr/MainActivity.kt
+++ AndroidApp/app/src/main/java/pl/todoit/IndustrialWebViewWithQr/MainActivity.kt
@@ -4,25 +4,37 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.FrameLayout
-import android.widget.LinearLayout
-import androidx.fragment.app.FragmentContainer
 
 class MainActivity : AppCompatActivity() {
+    val webviewFragmentName = "base"
+    val scanQrFragmentName = "popup"
+
+    fun setQrPopup() : ScanQrFragment {
+        var popupViewSetFragmentTran = supportFragmentManager.beginTransaction()
+        var scanQrFrag = ScanQrFragment()
+        popupViewSetFragmentTran.add(R.id.popup_fragment, scanQrFrag, scanQrFragmentName)
+        popupViewSetFragmentTran.commit()
+        return scanQrFrag
+    }
+
+    fun unsetQrPopup() {
+        val frag = supportFragmentManager.findFragmentByTag(scanQrFragmentName)
+
+        if (frag == null) {
+            return
+        }
+
+        supportFragmentManager.beginTransaction().remove(frag).commit()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         var baseViewSetFragmentTran = supportFragmentManager.beginTransaction()
         var webViewFrag = WebViewFragment()
-        baseViewSetFragmentTran.add(R.id.base_fragment, webViewFrag, "base")
+        baseViewSetFragmentTran.add(R.id.base_fragment, webViewFrag, webviewFragmentName)
         baseViewSetFragmentTran.commit()
-
-        var popupViewSetFragmentTran = supportFragmentManager.beginTransaction()
-        var scanQrFrag = ScanQrFragment()
-        popupViewSetFragmentTran.add(R.id.popup_fragment, scanQrFrag, "popup")
-        popupViewSetFragmentTran.commit()
 
         var app = application
 
@@ -32,8 +44,9 @@ class MainActivity : AppCompatActivity() {
 
         app.showScanQrImpl = { x:ScanRequest ->
             runOnUiThread({
+                val frag = setQrPopup()
                 var popupFrag = findViewById<ViewGroup>(R.id.popup_fragment)
-                scanQrFrag.setInput(popupFrag, x)
+                frag.setInput(popupFrag, x)
                 popupFrag.visibility = View.VISIBLE
             })
         }
@@ -42,6 +55,7 @@ class MainActivity : AppCompatActivity() {
             runOnUiThread({
                 var popupFrag = findViewById<ViewGroup>(R.id.popup_fragment)
                 popupFrag.visibility = View.GONE
+                unsetQrPopup()
             })
         }
 
